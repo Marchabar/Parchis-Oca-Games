e456eb0e843051009521bb0f945ba65e59305eda
Michael <michael-poelzl@outlook.com>
2022-11-07 20:08:44 +0100

    added PlayerMatchStats, PlayerStatsOca and rescpective connections

---
+++ src/main/java/com/japarejo/springmvc/player/PlayerMatchStats.java
@@ -0,0 +1,47 @@
+package com.japarejo.springmvc.player;
+
+import com.japarejo.springmvc.match.Match;
+import com.japarejo.springmvc.user.User;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
+
+@Getter
+@Setter
+@Entity
+
+public class PlayerMatchStats {
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  @Column(unique=true, nullable=false)
+  private int id;
+
+  @PositiveOrZero //can be 0, if game quit
+  @Column(name="numTurnsPlayer")
+  private Integer numTurnsPlayer;
+
+  @PositiveOrZero //can be 0, if game quit
+  @Column(name="numDiceRolls")
+  private Integer numDiceRolls;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  @Column(name="playerColor")
+  private PlayerColor playerColor;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "match_stat_id")
+  private Match match;
+
+
+}
