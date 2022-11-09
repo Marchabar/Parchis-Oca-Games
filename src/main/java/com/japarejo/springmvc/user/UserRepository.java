e6fcd0e72d3ded424cfae83b4ff1d82716e66e36
JMGarCas <jmgarciacasillas135@gmail.com>
2022-11-09 10:59:13 +0100

    Improved Register

--- src/main/java/com/japarejo/springmvc/user/UserRepository.java
+++ src/main/java/com/japarejo/springmvc/user/UserRepository.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
     @Query("SELECT status FROM UserStatusEnum status")
-    List<UserStatusEnum> findStatus() throws DataAccessException;
+    List<UserStatusEnum> findStatus();
+    @Query("SELECT status FROM UserStatusEnum status WHERE status.id= ?1")
+    UserStatusEnum findStatusById(Integer id) throws DataAccessException;
     @Query("SELECT u FROM User u WHERE u.login= ?1")
     User findUsername(@Param("wa") String wa) throws DataAccessException;
     List<User> findAll();
