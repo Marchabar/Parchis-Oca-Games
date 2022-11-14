cbaa3d681d93a5c42f685b096e4ea7cf229d8e33
enrcabmun <enrcabmun@alum.us.es>
2022-11-14 17:15:06 +0100

    Renamed file structure

---
+++ src/main/java/com/ling1/springmvc/user/User.java
@@ -0,0 +1,36 @@
+package com.ling1.springmvc.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(unique=true, nullable=false, precision = 10)
+	private Integer id;
+
+    @Column(unique=true, nullable = false)
+    private String login;
+    
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name="status_id",nullable = false)
+    private UserStatusEnum userStatus;
+
+    @Column(nullable = false)
+    private String role;
+
+}
