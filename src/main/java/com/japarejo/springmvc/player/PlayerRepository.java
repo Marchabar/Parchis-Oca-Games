8b7963fb0be7c0bd2b891e867cd1c34f3f14e46c
Felixo <felixangelgudiel@gmail.com>
2022-11-14 16:43:48 +0100

    playerstats fixed

--- src/main/java/com/japarejo/springmvc/player/PlayerRepository.java
+++ src/main/java/com/japarejo/springmvc/player/PlayerRepository.java
@@ -8,15 +8,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlayerRepository extends CrudRepository<PlayerMatchStats, Integer>{
+public interface PlayerRepository extends CrudRepository<PlayerStats, Integer>{
 
-    @Query("SELECT s FROM PlayerMatchStats s where s.match.id = ?1")
-    List<PlayerMatchStats> findAllStatsForMatch(Integer matchID);
+    @Query("SELECT s FROM PlayerStats s where s.user.id = ?1")
+    List<PlayerStats> findAllStatsForUser(Integer userID);
+    List<PlayerStats> findAll();
 
-    @Query("SELECT s FROM PlayerMatchStats s where s.user.id = ?1")
-    List<PlayerMatchStats> findAllStatsForUser(Integer userID);
-
-    @Query("SELECT s FROM PlayerMatchStats s where s.user.id = ?1 AND s.match.id = ?2")
-    PlayerMatchStats findStatsForUserAndMatch(Integer userID, Integer matchID);
-    
 }
