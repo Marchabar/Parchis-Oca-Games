c1debfcfdf8504e332b03dcf98c6ee009cdc07ec
Marchabar <marchabar1@alum.us.es>
2022-11-15 20:27:16 +0100

    ocaBoard try

---
+++ src/main/java/com/ling1/springmvc/ocaBoard/OcaRepository.java
@@ -0,0 +1,7 @@
+package com.ling1.springmvc.ocaBoard;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OcaRepository extends CrudRepository<OcaBoard,Integer> {
+    
+}
