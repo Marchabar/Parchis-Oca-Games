b9eef28fc2aeb556f475a68b65de5292fa356b88
Marchabar <marchabar1@alum.us.es>
2022-11-15 20:50:55 +0100

    ocaBoard try

--- src/main/java/com/ling1/springmvc/welcome/WelcomeController.java
+++ src/main/java/com/ling1/springmvc/welcome/WelcomeController.java
@@ -1,26 +1,12 @@
 package com.ling1.springmvc.welcome;
 
-import java.util.Date;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.ling1.springmvc.ocaBoard.OcaService;
-
 @Controller
 public class WelcomeController {
-    @Autowired
-    OcaService ocaService;
-
-    @GetMapping(path = {"","/welcome"})   
-    public String welcome(Map<String, Object> model, HttpServletResponse response){
-        //response.addHeader("Refresh", "1");
-        model.put("now", new Date());
-        model.put("ocaBoard", ocaService.findById(1).get());
+    @GetMapping(path = {"","/"})   
+    public String welcome(){
         return "welcome";
     }
 }
