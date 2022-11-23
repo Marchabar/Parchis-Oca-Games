ef32dd0fd42f4a636a1453a98dd4df1ea3054bd3
FelixoGudiel <felixangelgudiel@gmail.com>
2022-11-23 12:42:15 +0100

    turns and advancing start

--- src/main/java/com/ling1/springmvc/match/MatchService.java
+++ src/main/java/com/ling1/springmvc/match/MatchService.java
@@ -1,19 +1,25 @@
 package com.ling1.springmvc.match;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ling1.springmvc.player.PlayerRepository;
+import com.ling1.springmvc.player.PlayerStats;
+
 @Service
 public class MatchService {
 
     private MatchRepository matchRepo;
+    private PlayerRepository playerRepo;
 
     @Autowired
-    public MatchService(MatchRepository matchRepo){
+    public MatchService(MatchRepository matchRepo, PlayerRepository playerRepo){
         this.matchRepo = matchRepo;
+        this.playerRepo = playerRepo;
     }
 
     @Transactional(readOnly = true)
@@ -29,7 +35,22 @@ public class MatchService {
     public List<Match> findMatchesByLobbyId(Integer id){
         return matchRepo.findMatchesByLobbyId(id);
     }
-
-
+    @Transactional(readOnly = true)
+    public Match getMatchById(int id) {
+	    Optional<Match> result=matchRepo.findById(id);
+	    return result.isPresent()?result.get():null;         
+    }
+    @Transactional(readOnly = true)
+    public PlayerStats findPlayer(Integer match_id,Integer player_id){
+        Match m = matchRepo.findById(match_id).isPresent()?matchRepo.findById(match_id).get():null;
+        if (m!=null){
+            for (PlayerStats ps :m.getPlayerStats()){
+                if (ps.getUser()==playerRepo.findById(player_id).get().getUser()){
+                    return ps;
+                }
+            }
+        }
+        return null;
+    }
     
 }
--- src/main/java/com/ling1/springmvc/player/PlayerService.java
+++ src/main/java/com/ling1/springmvc/player/PlayerService.java
@@ -8,6 +8,8 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
+
 @Service
 public class PlayerService {
 
