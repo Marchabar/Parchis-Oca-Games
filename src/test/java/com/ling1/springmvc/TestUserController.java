bd0f8833b6bf0d55a7e86ceb46fe7c483fa868a5
mipo17 <michael-poelzl@outlook.com>
2022-12-06 18:12:05 +0100

    added controller test in TestUserController

--- src/test/java/com/ling1/springmvc/TestUserController.java
+++ src/test/java/com/ling1/springmvc/TestUserController.java
@@ -1,14 +1,20 @@
 package com.ling1.springmvc;
 
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ling1.springmvc.user.User;
+import com.ling1.springmvc.user.UserStatusEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,6 +33,7 @@ import com.ling1.springmvc.configuration.SecurityConfiguration;
 import com.ling1.springmvc.user.UserController;
 import com.ling1.springmvc.user.UserService;
 
+import org.assertj.core.util.Lists;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(value = UserController.class,
@@ -41,16 +48,110 @@ public class TestUserController {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void testUserController() throws Exception {
-        testRegisterUser();
+    private static final int TEST_USER_ID = 1;
+
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    void setup() {
+        user1 = new User();
+        user1.setId(TEST_USER_ID);
+        user1.setLogin("pedro");
+        user1.setPassword("123");
+        user1.setRole("admin");
+        UserStatusEnum mm = new UserStatusEnum();
+        mm.setName("Online");
+        user1.setUserStatus(mm);
+
+        user2 = new User();
+        user2.setId(2);
+        user2.setLogin("sandra");
+
+        given(this.userService.getAllUsers()).willReturn(Lists.newArrayList(user1,user2));
+        given(this.userService.getUserById(1)).willReturn(user1);
+
+        //given(this.ownerService.findOwnerById(TEST_OWNER_ID)).willReturn(new Owner());
+        //given(this.petService.findPetById(TEST_PET_ID)).willReturn(new Pet());
     }
 
-    void testRegisterUser() throws Exception{
+    @Test
+    void testUserController() throws Exception {
+        testGetShowUsersListing();
+        testGetDeleteUser();
+        testGetEditUser();
+        testPostEditUser();
+        testGetCreateUser();
+        testPostSaveNewUser();
+        testPostSaveNewRegisteredUser();
+        testGetRegisterUser();
+    }
+    // TODO ask prof how to deal with lists when expecting values, not done in his examples
+    void testGetShowUsersListing() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("users"))
+                //.andExpect(model().attribute("login",hasProperty("pedro")))
+                //.andExpect(model().attribute("login",hasProperty("sandra")))
+                .andExpect(view().name("Users/UsersListing"));
+    }
+    // problem here actual value 404 - makes sense since /delete/1/ does not exist
+    void testGetDeleteUser() throws Exception {
+        mockMvc.perform(get("/users/delete/{id}",TEST_USER_ID))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("message",is("User removed successfully")));
+
+    }
+    // TODO ask regarding ENUM how to deal with that
+    void testGetEditUser() throws Exception {
+        UserStatusEnum e = new UserStatusEnum();
+        e.setName("Online");
+        mockMvc.perform(get("/users/edit/{id}",TEST_USER_ID))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("user",hasProperty("login", is("pedro"))))
+                .andExpect(model().attribute("user",hasProperty("password", is("123"))))
+                .andExpect(model().attribute("user",hasProperty("role", is("admin"))))
+                //.andExpect(model().attribute("user",hasProperty("userStatus", is(e))))
+                ;
+
+    }
+    void testPostEditUser() throws Exception {
+        mockMvc.perform(post("/users/edit/{id}",TEST_USER_ID)
+                .with(csrf())
+                .param("login","luis")
+                .param("password","555"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("message",is("User saved successfully!")));
+    }
+    void testGetCreateUser()throws Exception {
+        mockMvc.perform(get("/users/create"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Users/EditUser"));
+    }
+    void testPostSaveNewUser() throws Exception {
+        UserStatusEnum enums = new UserStatusEnum();
+        enums.setName("Online");
+        mockMvc.perform(post("/users/create")
+                .with(csrf())
+                .param("login","alejandro")
+                .param("password","465")
+                .param("role","member"))
+                //.param("userStatus",enums.getName())) // TODO ask regarding enums
+                .andExpect(model().attribute("message",is("User saved successfully")))
+                .andExpect(status().isOk());
+    }
+
+    void testGetRegisterUser() throws Exception{
+        mockMvc.perform(get("/users/register"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(status().isOk());
+    }
+    void testPostSaveNewRegisteredUser() throws Exception{
         mockMvc.perform(post("/users/register")
-                            .with(csrf())
-                            .param("login", "realdtrump")
-                            .param("password", "MAGA"))
+                        .with(csrf())
+                        .param("login", "realdtrump")
+                        .param("password", "MAGA"))
                 .andExpect(status().isOk());
     }
 }
