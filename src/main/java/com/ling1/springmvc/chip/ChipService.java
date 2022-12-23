457d4f26d57a7f63886c737d37ac0e6ac9a9ab4b
FelixoGudiel <felixangelgudiel@gmail.com>
2022-12-23 21:54:55 +0100

    chip entity, parchis init, oca separation

---
+++ src/main/java/com/ling1/springmvc/chip/ChipService.java
@@ -0,0 +1,19 @@
+package com.ling1.springmvc.chip;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ChipService {
+    
+    @Autowired
+	private ChipRepository chipRepo;
+
+    @Transactional 
+	public Chip save(Chip l) {
+	    return chipRepo.save(l);
+	}
+}
