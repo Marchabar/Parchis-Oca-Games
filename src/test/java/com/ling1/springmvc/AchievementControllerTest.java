e3ed1e049138d9caffaaaed07dff915cffafa1eb
mipo17 <michael-poelzl@outlook.com>
2023-01-12 12:02:15 +0100

    update/added pos+neg tests

--- src/test/java/com/ling1/springmvc/AchievementControllerTest.java
+++ src/test/java/com/ling1/springmvc/AchievementControllerTest.java
@@ -4,6 +4,7 @@ import com.ling1.springmvc.achievements.Achievement;
 import com.ling1.springmvc.achievements.AchievementController;
 import com.ling1.springmvc.achievements.AchievementService;
 import com.ling1.springmvc.achievements.AchievementType;
+import com.ling1.springmvc.achievements.AchievementTypeFormatter;
 import com.ling1.springmvc.configuration.SecurityConfiguration;
 import com.ling1.springmvc.friend.Friend;
 import com.ling1.springmvc.friend.FriendController;
@@ -11,6 +12,7 @@ import com.ling1.springmvc.friend.FriendService;
 import com.ling1.springmvc.lobby.LobbyService;
 import com.ling1.springmvc.match.Match;
 import com.ling1.springmvc.match.MatchService;
+import com.ling1.springmvc.player.PlayerColor;
 import com.ling1.springmvc.player.PlayerService;
 import com.ling1.springmvc.player.PlayerStats;
 import com.ling1.springmvc.user.User;
@@ -30,6 +32,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -62,6 +65,9 @@ public class AchievementControllerTest {
     @MockBean
     private FriendService friendService;
 
+    @MockBean
+    AchievementTypeFormatter form; //needed for enum validation
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -73,11 +79,14 @@ public class AchievementControllerTest {
 
     private List<PlayerStats> allStats = new ArrayList<PlayerStats>();
     private PlayerStats playerStats;
-    private List<Achievement> achievements = new ArrayList<Achievement>();
-    private Achievement achievement;
+    private PlayerColor color;
+    private Achievement achievement = new Achievement();
+
+    private List<Achievement> allAchievements = new ArrayList<Achievement>();
+    private List<Achievement> myAchievements = new ArrayList<Achievement>();
 
     @BeforeEach
-    void setup(){
+    void setup() {
 
         user1 = new User();
         user1.setId(TEST_USER_ID);
@@ -98,25 +107,100 @@ public class AchievementControllerTest {
         user2.setUserStatus(us2stat);
 
         AchievementType achievementType = new AchievementType();
-        achievementType.setName("Won ALL");
+        achievementType.setName("DICE");
         achievementType.setId(1);
 
-        Achievement achievement = new Achievement();
+        achievement = new Achievement();
         achievement.setDescription("you only get if you are pro");
-        achievement.setName("DICE");
+        achievement.setName("Roller 25");
         achievement.setValue(25);
         achievement.setFileImage("someFileImage");
         achievement.setAchievementType(achievementType);
+        allAchievements.add(achievement);
 
         playerStats = new PlayerStats();
-        playerStats.setId(1);
-        playerStats.setNumDiceRolls(150);
-        allStats.add(playerStats);
+        color = new PlayerColor();
+        color.setName("red");
 
 
     }
+    @Test
+    void testGetShowAchievementsListing() throws Exception
+    {
 
-   
+        playerStats.setPlayerColor(color);
+        playerStats.setNumDiceRolls(30);
+        allStats.add(playerStats);
+
+        given(this.userService.findUsername(anyString())).willReturn(user1,user1);
+        given(this.playerService.giveAllStatsForPlayer(1)).willReturn(allStats);
+        given(this.achievementService.getAllAchievements()).willReturn(allAchievements);
+
+        myAchievements.add(achievement);
+
+        mockMvc.perform(get("/achievements/{username}",TEST_USER_NAME))
+        .andExpect(status().isOk())
+        .andExpect(model().attribute("achievements", is(allAchievements)))
+        .andExpect(model().attribute("myAchievements", is(myAchievements)))
+        .andExpect(model().attribute("loggedUser", is(user1)))
+        .andExpect(model().attribute("profUser", is(user1)))
+        .andExpect(view().name("Achievements/AchievementsListing"));
+    }
+    @Test
+    void testGetShow3AchievementsListing() throws Exception
+    {
+
+        playerStats.setPlayerColor(color);
+        playerStats.setNumDiceRolls(30);
+        playerStats.setNumberOfPlayerWells(50);
+        playerStats.setNumberOfPlayerPrisons(3);
+        allStats.add(playerStats);
+
+        /* add achievements */
+
+        AchievementType achievementTypeB = new AchievementType();
+        achievementTypeB.setName("WELL");
+        achievementTypeB.setId(1);
+
+        Achievement achievementB = new Achievement();
+        achievementB.setDescription("you only get if you are pro");
+        achievementB.setName("pro achievement");
+        achievementB.setValue(40);
+        achievementB.setFileImage("blablacar");
+        achievementB.setAchievementType(achievementTypeB);
+        allAchievements.add(achievementB);
+
+        AchievementType achievementTypeC = new AchievementType();
+        achievementTypeC.setName("PRISON");
+        achievementTypeC.setId(1);
+
+        Achievement achievementC = new Achievement();
+        achievementC.setDescription("you only get if you are prison lover");
+        achievementC.setName("jail house rock");
+        achievementC.setValue(2);
+        achievementC.setFileImage("thisisit");
+        achievementC.setAchievementType(achievementTypeC);
+        allAchievements.add(achievementC);
+
+        /* add achievements */
+
+        given(this.userService.findUsername(anyString())).willReturn(user1,user1);
+        given(this.playerService.giveAllStatsForPlayer(1)).willReturn(allStats);
+        given(this.achievementService.getAllAchievements()).willReturn(allAchievements);
+
+        myAchievements.add(achievement);
+        myAchievements.add(achievementB);
+        myAchievements.add(achievementC);
+
+
+        mockMvc.perform(get("/achievements/{username}",TEST_USER_NAME))
+        .andExpect(status().isOk())
+        .andExpect(model().attribute("achievements", is(allAchievements)))
+        .andExpect(model().attribute("myAchievements", is(myAchievements)))
+        .andExpect(model().attribute("loggedUser", is(user1)))
+        .andExpect(model().attribute("profUser", is(user1)))
+        .andExpect(view().name("Achievements/AchievementsListing"));
+    }
     @Test
     void ntestGetShowAchievementsListingNoAchievements() throws Exception
     {
@@ -127,19 +211,69 @@ public class AchievementControllerTest {
         .andExpect(view().name("welcome"));
     }
 
-    //TODO add achievments first
     @Test
     void ntestGetShowAchievementsListingNoPermission() throws Exception
     {
         given(this.userService.findUsername(anyString())).willReturn(user2,user1);
-        PlayerStats total = new PlayerStats();
-        total.setNumberOfGooses(105);
         given(this.playerService.giveAllStatsForPlayer(1)).willReturn(allStats);
 
         mockMvc.perform(get("/achievements/{username}",TEST_USER_NAME))
         .andExpect(status().is3xxRedirection())
         .andExpect(model().attribute("message",is("You cannot access these achievements!")))
         .andExpect(view().name("redirect:/"));
+    }
+
+    @Test
+    void testGetCreateAchievement()throws Exception
+    {
+        mockMvc.perform(get("/achievements/create"))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeExists("achievement"))
+        .andExpect(view().name("Achievements/EditAchievement"));
+    }
+
+    @Test
+    void testPostreateAchievement()throws Exception, ParseException
+    {
+        AchievementType achievementType = new AchievementType();
+        achievementType.setName("GOOSE");
+        achievementType.setId(1);
+
+        given(this.form.parse(anyString(),any())).willReturn(achievementType); //needed for formatter in user
+
+        mockMvc.perform(post("/achievements/create")
+        .with(csrf())
+        .param("name","")
+        .param("fileImage","CustomAchievement")
+        .param("description","")
+        .param("achievementType","DICE")
+        .param("value","100"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(model().attribute("message",is("Achievement saved successfully")))
+        .andExpect(view().name("redirect:/achievements"));
+    }
+
+    @Test
+    void ntestPostreateAchievementAlreadyExists()throws Exception, ParseException
+    {
+        AchievementType achievementType = new AchievementType();
+        achievementType.setName("DICE");
+        achievementType.setId(1);
+
+        given(this.form.parse(anyString(),any())).willReturn(achievementType); //needed for formatter in user
+        given(this.achievementService.getAllAchievements()).willReturn(allAchievements);
+
+
+        mockMvc.perform(post("/achievements/create")
+        .with(csrf())
+        .param("name","")
+        .param("fileImage","CustomAchievement")
+        .param("description","")
+        .param("achievementType","DICE")
+        .param("value","25"))
+        .andExpect(status().isOk())
+        .andExpect(model().attribute("message",is(achievement.getName() + " already exists!")))
+        .andExpect(view().name("Achievements/EditAchievement"));
     }
     
 }
